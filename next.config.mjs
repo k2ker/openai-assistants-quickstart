4e27112b040e6097463055a67f3e098230733a27
HYEONG\k2ker <kkerkk9@gmail.com>
2025-02-18 19:38:15 +0900

    개발 진행중

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  //사이트 접속 시 /examples/basic-chat 페이지로 이동
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/examples/basic-chat",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
