972f561be7d3e9184e8a7190f5d7c4bb188f4bb6
HYEONG\k2ker <kkerkk9@gmail.com>
2025-02-18 21:13:12 +0900

    env 수정

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   output: "standalone",
   env: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY, // ✅ 환경 변수를 빌드 타임에 강제 적용
+    OPENAI_ASSISTANT_ID: process.env.OPENAI_ASSISTANT_ID,
   },
   //사이트 접속 시 /examples/basic-chat 페이지로 이동
   redirects: async () => {
